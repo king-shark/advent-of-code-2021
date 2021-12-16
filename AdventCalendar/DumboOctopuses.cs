429ec8b160d62f1d3fb26ccf5a11eaa208005d28
Greg Shark <kingfelix@gmail.com>
2021-12-16 00:27:52 -0600

    Day 11 Solution Added

---
+++ AdventCalendar/DumboOctopuses.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace AdventCalendar
+{
+    public class DumboOctopuses
+    {
+        int[,] octopi = new int[10, 10];
+        List<Coord> flashers = new List<Coord>();
+        public long tempCount = 0;
+        public long FlashCount = 0;
+        public long StepCount = 0;
+
+        public DumboOctopuses(string filename, int stepCount = 0)
+        {
+            string line;
+            StreamReader reader = File.OpenText(filename);
+            int i = 0;
+            int j = 0;
+            while ((line = reader.ReadLine()) != null)
+            {
+                j = 0;
+                foreach (var c in line.ToArray())
+                {
+                    octopi[i, j] = Int32.Parse(c.ToString());
+                    j++;
+                }
+                i++;
+            }
+            DoSteps(stepCount);
+            FlashCount = tempCount;
+            while (flashers.Count != 100)
+            {
+                ContinueSteps();
+            }
+        }
+        public void ContinueSteps()
+        {
+            DoSteps(1);
+        }
+        public void DoSteps(int stepCount)
+        {
+            for (int sc = 0; sc < stepCount; sc++)
+            {
+                StepCount++;
+                Step();
+            }
+        }
+        public void Step()
+        {
+            IncrementValues();
+            DoFlash();
+            Reset();
+        }
+        public void IncrementValues()
+        {
+            flashers = new List<Coord>();
+            for (int i = 0; i < octopi.GetLength(0); i++)
+            {
+                for (int j = 0; j < octopi.GetLength(1); j++)
+                {
+                    octopi[i, j]++;
+                    if (octopi[i, j] > 9) { AddFlasher(new Coord(i, j)); }
+                }
+            }
+        }
+        public void DoFlash()
+        {
+            for (int c = 0; c < flashers.Count; c++)
+            {
+                tempCount++;
+                UpdateNeighbors(flashers[c]);
+            }
+        }
+        public void Reset()
+        {
+            foreach (Coord coord in flashers)
+            {
+                octopi[coord.x, coord.y] = 0;
+            }
+        }
+        public void AddFlasher(Coord coord)
+        {
+            if (!flashers.Exists(c => c.x == coord.x && c.y == coord.y)) { flashers.Add(coord); }
+        }
+        public void UpdateNeighbors(Coord coord)
+        {
+            foreach (Coord neighbor in coord.Neighbors)
+            {
+                if (neighbor.x >= 0 && neighbor.x < octopi.GetLength(0) && neighbor.y >= 0 && neighbor.y < octopi.GetLength(1))
+                {
+                    octopi[neighbor.x, neighbor.y]++;
+                    if (octopi[neighbor.x, neighbor.y] > 9) { AddFlasher(neighbor); }
+                }
+            }
+        }
+    }
+
+    public class Coord
+    {
+        private string val = "";
+        public List<Coord> Neighbors
+        {
+            get
+            {
+                List<Coord> newList = new List<Coord>();
+                newList.Add(new Coord(x - 1, y - 1));
+                newList.Add(new Coord(x - 1, y));
+                newList.Add(new Coord(x - 1, y + 1));
+                newList.Add(new Coord(x, y - 1));
+                newList.Add(new Coord(x, y + 1));
+                newList.Add(new Coord(x + 1, y - 1));
+                newList.Add(new Coord(x + 1, y));
+                newList.Add(new Coord(x + 1, y + 1));
+                return newList;
+            }
+        }
+        public int x
+        {
+            get
+            {
+                return Int32.Parse(val.Split(',')[0]);
+            }
+        }
+        public int y
+        {
+            get
+            {
+                return Int32.Parse(val.Split(',')[1]);
+            }
+        }
+        public Coord(int x, int y)
+        {
+            val = x + "," + y;
+        }
+    }
+}
--- AdventCalendar/Program.cs
+++ AdventCalendar/Program.cs
@@ -7,27 +7,33 @@ namespace AdventCalendar
     {
         static void Main(string[] args)
         {
+            //Day 1
             SonarSweep puzzle1 = new SonarSweep("AdventPuzzleInput1.txt");
             Debug.WriteLine("Day 1: The answer you seek is " + puzzle1.AnalyzeSweepData(1) + ".");
             Debug.WriteLine("Day 1: The answer to puzzle two is " + puzzle1.AnalyzeSweepData(3) + ".");
 
+            //Day 2
             NavigationSystems puzzle2 = new NavigationSystems("AdventPuzzleInput2.txt");
             Debug.WriteLine("Day 2: This is a tricky one. But for puzzle three, the answer be: " + puzzle2.CalculateFlightPath(false));
             Debug.WriteLine("Day 2: Well, turns out that last answer was rubbish. We needed more data! I now calculate our current position to be: " + puzzle2.CalculateFlightPath(true));
 
+            //Day 3
             SubmarineDiagnostics puzzle3 = new SubmarineDiagnostics("AdventPuzzleInput3.txt");
             Debug.WriteLine("Day 3: Moving on to Day 3, Puzzle number one's data input says that the power consumption of the submarine is: " + puzzle3.PowerConsumption());
             Debug.WriteLine("Day 3: Furthermore, based on my exquisite calculations, the life support rating of our submarine is: " + puzzle3.LifeSupportRating());
 
+            //Day 4
             Bingo puzzle4 = new Bingo("AdventPuzzleInput4.txt",5,5);
             Debug.WriteLine("Day 4: Bingo with a squid then. Alright. If I am to win, I must pick the board with a final score of " + puzzle4.PlayBingo());
             Debug.WriteLine("Day 4: However, might it not be prudent to let the beast win? Perhaps I should pick the board guaranteed to lose. It would be the one with a final score of " + puzzle4.LoseBingo());
 
+            //Day 5
             GeoThermalVentMap puzzle5 = new GeoThermalVentMap("AdventPuzzleInput5.txt", 1000,1000, false);
             Debug.WriteLine("Day 5: Great, now there's geothermal vents. Well, no matter. If I comb the map data, it looks like there are only {0} points I need to avoid. Easy.", puzzle5.GetVentDensity(2));
             puzzle5 = new GeoThermalVentMap("AdventPuzzleInput5.txt", 1000, 1000, true);
             Debug.WriteLine("Day 5: Did I seriously forget to include diagonal vents?? Now it looks like there are {0} points I need to avoid. Only slightly less easy.", puzzle5.GetVentDensity(2));
-            
+
+            //Day 6
             LanternFishStudy puzzle6 = new LanternFishStudy("AdventPuzzleInput6.txt", 7, 2);
             int days = 80;
             puzzle6.UpdateFish(days);
@@ -38,29 +44,39 @@ namespace AdventCalendar
             Debug.WriteLine("Day 6: By Santa's Beard... If these fish were to find a way to acheive immortality, then after a mere {0} days, there would be {1} of them!", days, puzzle6.GetTotal());
             Debug.WriteLine("I must find Santa's sleigh keys so I can inform him of this potential threat to Christmas!");
 
+            //Day 7
             CrabSubmarine puzzle7 = new CrabSubmarine("AdventPuzzleInput7.txt");
             Debug.WriteLine("Day 7: It is difficult to concentrate over the sound of the alarms but I have them to thank for having avoided becoming lunch for a whale! These crabs in submarines seem to be trying to help me.");
             Debug.WriteLine("All I need to do is get them to move to the position where the sum is: {0}", puzzle7.GetMostFuelEfficientPosition());
             Debug.WriteLine("Day 7: The crabs are dissatisfied with my results. It looks like my assumptions about crab submarine fuel consumption were wrong.");
             Debug.WriteLine("After a brief update to my code, I have determined that the correct position to move to is: {0}", puzzle7.GetCrabMostFuelEfficientPosition());
 
+            //Day 8
             SevenSegmentDisplay puzzle8 = new SevenSegmentDisplay("AdventPuzzleInput8.txt");
             Debug.WriteLine("Day 8: Now the display is acting up. Let's see, I think I can translate the garbled output into something usable.");
             Debug.WriteLine("I think the number of times 1,4, 7 and 8 appear is {0}", puzzle8.GetEasyNumberCount());
             Debug.WriteLine("Day 8: I think I've cracked a way to read the output. If I'm right, the total of all the output digits should be {0}.", puzzle8.GetOutputSum());
 
+            //Day 9
             //LavaTubes puzzle9 = new LavaTubes("AdventPuzzleInput9.txt");
             //Debug.WriteLine("Day 9: Now that I've got my bearings, it seems I have entered a series of interconnected lava tubes. The smoke is collected at points with the lowest elevation compared to surrounding points.");
             //Debug.WriteLine("It looks like the total height of these points is: {0}", puzzle9.GetLowPoints());
 
+            //Day 10
             NavigationSubsystem puzzle10 = new NavigationSubsystem("AdventPuzzleInput10.txt");
-            Debug.WriteLine("The syntax error score is: {0}", puzzle10.syntaxErrorScore);
-            Debug.WriteLine("The autocomplete score is: {0}", puzzle10.GetAutocompleteScore());
+            Debug.WriteLine("Day 10: The syntax error score is: {0}", puzzle10.syntaxErrorScore);
+            Debug.WriteLine("Day 10: The autocomplete score is: {0}", puzzle10.GetAutocompleteScore());
+
+            //Day 11
+            int steps = 100;
+            DumboOctopuses puzzle11 = new DumboOctopuses("AdventPuzzleInput11.txt", steps);
+            Debug.WriteLine("Day 11: After {0} steps, I count {1} flashes.", steps, puzzle11.FlashCount);
+            Debug.WriteLine("Day 11: It looks like on step {0} the flashes will synchronize.", puzzle11.StepCount);
 
             //Day 12
             PassagePathing puzzle12 = new PassagePathing("AdventPuzzleInput12.txt");
-            Debug.WriteLine("Number of paths to the end that traverse small nodes only once is " + puzzle12.PathCount);
-            Debug.WriteLine("Number of paths to the end that traverse one small node twice and the rest only once is " + puzzle12.PathCount2);
+            Debug.WriteLine("Day 12: Number of paths to the end that traverse small nodes only once is " + puzzle12.PathCount);
+            Debug.WriteLine("Day 12: Number of paths to the end that traverse one small node twice and the rest only once is " + puzzle12.PathCount2);
         }
     }
 }
